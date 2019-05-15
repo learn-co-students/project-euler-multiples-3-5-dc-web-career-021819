6b72e611d9cec83ff51957cd687cdeae4c9cadfd
Sean <shonkelli@gmail.com>
2019-05-15 10:30:55 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+
+# create an empty array
+output = []
+# iterate through all numbers between 1 and 1000
+# and add multiples of 3 and 5 to the array
+(1..1000).to_a.each { |num| output.push(num) if num % 3 == 0 || num % 5 == 0}
+# sum everything in the array
+output.reduce(:+)
